e8d004da53ea91c3d1041c84707551a0286b4399
Kiryl Zhurauleu <FirstKnight@list.ru>
2019-03-20 10:50:31 +0300

    bootstrap4

--- homework5/script1.js
+++
@@ -1,7 +0,0 @@
-function changeStyle(){
-              document.getElementById('botton2').style.border = '1px solid red';
-              document.getElementById('botton3').style.color = 'yellow';
-              document.getElementById('botton1').style.size = '20px';
-
-
-           }
